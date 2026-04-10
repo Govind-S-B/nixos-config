6cc96199035aea941daa829590d040a48b183e02
Govind S B <b.s.dnivog@gmail.com>
2026-04-10 19:29:06 +0530

    basic working system

--- flake.nix
+++ flake.nix
@@ -5,13 +5,18 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, sops-nix, ... }: {
     nixosConfigurations.nixchan = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        sops-nix.nixosModules.sops
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
--- home.nix
+++ home.nix
@@ -1,20 +1,43 @@
 { config, pkgs, ... }:
 
 {
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.11";
+
+  home.sessionPath = [ "$HOME/.npm-global/bin" ];
+
+  home.sessionVariables = {
+    ANTHROPIC_BASE_URL = "https://openrouter.ai/api";
+    ANTHROPIC_API_KEY = "";
+    CLAUDE_CODE_DISABLE_AUTOUPDATE = "1";
+  };
 
   home.shellAliases = {
     rebuild = "cd ~/nixos-config && git add . && sudo nixos-rebuild switch --flake .#nixchan";
   };
 
+  # Wrapper scripts that inject secrets only into the launched process
+  # Secrets are read from /run/secrets/ (decrypted by sops-nix at activation)
+  home.packages = with pkgs; [
+    nodejs
+    (writeShellScriptBin "ccfree" ''
+      export ANTHROPIC_AUTH_TOKEN=$(cat /run/secrets/anthropic_auth_token)
+      exec claude --model openrouter/free --dangerously-skip-permissions "$@"
+    '')
+  ];
+
   programs.bash.enable = true;
 
   programs.git = {
     enable = true;
-    signing.format = null;
     settings.user = {
       name = "Govind S B";
       email = "b.s.dnivog@gmail.com";
     };
+  };
+
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    terminal = "screen-256color";
   };
 }
